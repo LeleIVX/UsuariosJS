b90220ba5c0ecdf300d5db1135cca3fbd8572bfe
leandro leiva <leandro.s.leiva@hotmail.com>
2022-07-03 21:49:20 -0300

    Registro de usuarios

---
+++ registros.js
@@ -0,0 +1,17 @@
+alert ("Bienvenidx al sitio");
+
+let registro= prompt("Querés registrarte? SI/NO");
+if (registro == "SI"){
+    for(i =0; i<10; i++){
+        let= user + i == prompt("Ingrese su nombre de usuario");
+        console.log (user+i);
+        let= nombre = prompt("Ingrese su nombre");
+        let= apellido = prompt("Ingrese su apellido");
+        let= mail = prompt("Ingrese su mail");
+        let= genero = prompt("Ingrese su género");
+        let fin = prompt ("Terminaste tu registro? SI/NO");
+        if(fin == "SI"){
+            break;
+        }
+    }
+};
