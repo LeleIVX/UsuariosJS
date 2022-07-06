86ac03e3a6d196c4bbf02d3e85ab6541ce3bae7b
leandro leiva <leandro.s.leiva@hotmail.com>
2022-07-06 20:53:34 -0300

    Arreglos de usuarios

--- registros.js
+++ registros.js
@@ -1,14 +1,17 @@
 alert ("Bienvenidx al sitio");
 
+let lista_usuarios = [];
 let registro= prompt("Querés registrarte? SI/NO");
 if (registro == "SI"){
     for(i =0; i<10; i++){
-        let= user + i == prompt("Ingrese su nombre de usuario");
-        console.log (user+i);
-        let= nombre = prompt("Ingrese su nombre");
-        let= apellido = prompt("Ingrese su apellido");
-        let= mail = prompt("Ingrese su mail");
-        let= genero = prompt("Ingrese su género");
+        let user = prompt("Ingrese su nombre de usuario");
+        let nombre = prompt("Ingrese su nombre");
+        let apellido = prompt("Ingrese su apellido");
+        let mail = prompt("Ingrese su mail");
+        let genero = prompt("Ingrese su género");
+        let nuevo_usuario = new usuario (user, nombre, apellido, mail, genero);
+        lista_usuarios.push(nuevo_usuario);
+
         let fin = prompt ("Terminaste tu registro? SI/NO");
         if(fin == "SI"){
             break;
