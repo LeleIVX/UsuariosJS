6e0bbaa34abf64d42317265b36a204155e7dcb8d
leandro leiva <leandro.s.leiva@hotmail.com>
2022-07-07 08:47:09 -0300

    arreglos

--- registros.js
+++ registros.js
@@ -1,20 +1,47 @@
 alert ("Bienvenidx al sitio");
 
+class usuario{
+    constructor(id,user,nombre,apellido,mail,genero){
+        this.id = id;
+        this.user = user;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.mail = mail;
+        this.genero = genero;
+    }
+}
+
 let lista_usuarios = [];
 let registro= prompt("Querés registrarte? SI/NO");
 if (registro == "SI"){
     for(i =0; i<10; i++){
+        let id = i;
         let user = prompt("Ingrese su nombre de usuario");
         let nombre = prompt("Ingrese su nombre");
         let apellido = prompt("Ingrese su apellido");
         let mail = prompt("Ingrese su mail");
         let genero = prompt("Ingrese su género");
-        let nuevo_usuario = new usuario (user, nombre, apellido, mail, genero);
-        lista_usuarios.push(nuevo_usuario);
-
+        
         let fin = prompt ("Terminaste tu registro? SI/NO");
         if(fin == "SI"){
-            break;
+            let nuevo_usuario = new usuario (id,user, nombre, apellido, mail, genero);
+            lista_usuarios.push(nuevo_usuario);
+            let continuar = prompt ("Desea ingresar otro usuario? SI/NO")
+            if(continuar == "NO"){
+                break;
+            }
         }
     }
 };
+
+console.log (lista_usuarios);
+
+for(let usuario of lista_usuarios){
+    console.log ("ID ", usuario.id);
+    console.log ("Usuario ", usuario.user);
+    console.log ("Nombre ", usuario.nombre);
+    console.log ("Apellido ", usuario.apellido);
+    console.log ("Mail ", usuario.mail);
+    console.log ("Género ", usuario.genero);
+    console.log ("--------------");
+}
