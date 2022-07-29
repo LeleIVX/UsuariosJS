4d4882c84988cb8726e0eb4d1389e418a35da8aa
leandro leiva <leandro.s.leiva@hotmail.com>
2022-07-29 20:32:02 -0300

    DOM agregado

--- JS/iniciar_sesion.js
+++ JS/iniciar_sesion.js
@@ -11,8 +11,14 @@ bnt_inicio.addEventListener("click" ,function(e){
             alert("usuario no registrado")
         }*/
     let datos_user = document.getElementById("datos_user");
-    datos_user.innerHTML = "<h2>Bienvenido!</h2>";
+    datos_user.innerHTML = "<h2>Bienvenido!</h2><table class='table table-borderless'><tbody><tr><th>Usuario</th><td id='username'></td></tr><tr><th>ID</th><td id='id'></td></tr><tr><th>Nombre</th><td id='nombre'></td></tr><tr><th>Apellido</th><td id='apellido'></td></tr><tr><th>Género</th><td id='genero'></td></tr><tr><th>Email</th><td id='email'></td></tr></tbody></table>";
     datos_user.className = "container tabla-usuario";
+    document.getElementById("username").innerText = user;
+    document.getElementById("id").innerText = id_user;
+    document.getElementById("nombre").innerText = nombre;
+    document.getElementById("apellido").innerText = apellido;
+    document.getElementById("genero").innerText = genero;
+    document.getElementById("email").innerText = mail;
 })
 
 
