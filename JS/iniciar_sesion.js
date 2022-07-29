f68f0283fb2e75361c71df3b73888a59e0a5f61a
leandro leiva <leandro.s.leiva@hotmail.com>
2022-07-29 03:24:16 -0300

    error de logueo

--- JS/iniciar_sesion.js
+++ JS/iniciar_sesion.js
@@ -2,9 +2,9 @@ let bnt_inicio = document.getElementById("iniciar_sesion");
 
 bnt_inicio.addEventListener("click" ,function(e){
     e.preventDefault();
-    console.log ("Funciona")
-    let usuario_inicio = document.getElementById ("user_ingreso");
-    let usuario = localStorage.getItem ("LeleIVX");
-    console.log(JSON.parse(usuario));
+    console.log ("Funciona");
+
 })
 
+
+console.log (JSON.parse(localStorage.getItem("LeleIVX")));
