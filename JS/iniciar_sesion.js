640283ce1328bc9138a17c405ef816e44ba54590
leandro leiva <leandro.s.leiva@hotmail.com>
2022-08-01 06:08:20 -0300

    validando ingreso

--- JS/iniciar_sesion.js
+++ JS/iniciar_sesion.js
@@ -5,11 +5,19 @@ bnt_inicio.addEventListener("click" ,function(e){
     e.preventDefault();
     let username = document.getElementById("user_ingreso");
     let login = JSON.parse(localStorage.getItem(username.value));
+    if (login == null){
+        Swal.fire({
+            icon: 'error',
+            title: 'Nombre de usuario no registrado',
+            text: 'Deseas registrar un nuevo usuario?',
+            footer: '<a href="../index.html#titulo">Registrarme</a>'
+        })
+    }
+    
+
+        else{
     let {user,id_user,nombre,apellido,genero,mail} = login
     console.log(user,id_user,nombre,apellido,genero,mail);
-        /*if (login =  ){
-            alert("usuario no registrado")
-        }*/
     let datos_user = document.getElementById("datos_user");
     datos_user.innerHTML = "<h2>Bienvenido!</h2><table class='table table-borderless'><tbody><tr><th>Usuario</th><td id='username'></td></tr><tr><th>ID</th><td id='id'></td></tr><tr><th>Nombre</th><td id='nombre'></td></tr><tr><th>Apellido</th><td id='apellido'></td></tr><tr><th>Género</th><td id='genero'></td></tr><tr><th>Email</th><td id='email'></td></tr></tbody></table>";
     datos_user.className = "container tabla-usuario";
@@ -19,6 +27,6 @@ bnt_inicio.addEventListener("click" ,function(e){
     document.getElementById("apellido").innerText = apellido;
     document.getElementById("genero").innerText = genero;
     document.getElementById("email").innerText = mail;
-})
+}})
 
 
